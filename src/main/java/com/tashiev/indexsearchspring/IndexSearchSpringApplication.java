30bb16a6681178444f96a6f868691a0f0fd5c1f1
1Zholdoshbek <zholdoshbektashiev@gmail.com>
2025-01-31 11:42:25 +0600

    Initial commit

---
+++ src/main/java/com/tashiev/indexsearchspring/IndexSearchSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tashiev.indexsearchspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndexSearchSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndexSearchSpringApplication.class, args);
+    }
+
+}
